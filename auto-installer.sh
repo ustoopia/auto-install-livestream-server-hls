c9f75c214ca3c21da4b5a0716f9454d8ca8b23a8
ustoopia <ustoopia@gmail.com>
2024-01-30 16:20:18 +0100

    Update auto-installer.sh

--- auto-installer.sh
+++ auto-installer.sh
@@ -1,55 +1,62 @@
 #!/bin/bash
-# Check if script is run with sudo
+# ################################################################
+# Hi, my name is auto-install-livestream-server-hls. I live at   #
+# https://github.com/ustoopia/auto-install-livestream-server-hls #
+# I'm going to build you a fully configured live-stream server.  #
+# Lucky you! To really get me going: sudo bash auto-installer.sh #
+# ################################################################
+
+# I need to make sure you used to sudo to get me going
 if [ "$(id -u)" != "0" ]; then
-    echo "THIS SCRIPT SHOULD BE RUN BY SUDO. Please try using : sudo bash $0"
+    echo "THIS SCRIPT SHOULD BE RUN AS SUDO. Please try using: sudo bash $0"
     exit 1
 fi
 
-# Ask user to input their domain name
+# Asking the user, very politely, to enter their domain name for the server
 read -r -p "Please enter a working domain name that points to your server: " DOMAIN_NAME
 
-# Ask for the user's email address for certbot later on
-read -r -p "Please enter your email address (used only for certificates): " EMAIL_ADDRESS
+# Asking the user to enter their email address, also in a polite manner
+read -r -p "Please enter your email address. Used only for the certificate request: " EMAIL_ADDRESS
 
-# Temporary store domain name and email address
+# Temporary store the entered domain name and email address so we can use it later on
 echo "DOMAIN_NAME=$DOMAIN_NAME" | sudo tee /tmp/user_data > /dev/null
 echo "EMAIL_ADDRESS=$EMAIL_ADDRESS" | sudo tee -a /tmp/user_data > /dev/null
 
-# Set DEBIAN_FRONTEND to noninteractive to prevent prompts during apt upgrade
+# I want to run the entire scipt myself without bothering my user for confirmation prompts
 export DEBIAN_FRONTEND=noninteractive
 
-# Set up sudo without password
+# Temporary allow me to use sudo without having to ask my user to enter their password each time.
 echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
 
-# Update repositories and install apps we need for checking the IP's. Also perform general upgrade.
+# Updating repositories and installing apps I'm needing for the next steps. Also performing a general update.
 sudo apt-get update -y 
 sudo apt-get install curl dnsutils wget unzip git jq -y
 sudo apt-get upgrade -y
 
-# Validate the domain name and get its IP address using 1.1.1.1 DNS server
+# Validating the entered domain name and checking its IP address using the 1.1.1.1 DNS server
 DOMAIN_IP=$(dig +short @"1.1.1.1" "$DOMAIN_NAME")
 
-# Get the user's external IP address
+# Looking up what the internet facing IP address is of my current location
 USER_EXTERNAL_IP=$(curl -s https://api64.ipify.org?format=json | jq -r .ip)
 
-# Get the local external IP address
+# I also want to know our current local IP address, so I can compare the three IPs.
 LOCAL_EXTERNAL_IP=$(curl -s ifconfig.me)
 
-# Display the obtained IP addresses
+# A list showing the results of the IPs that I collected.
 echo "Domain IP: $DOMAIN_IP"
 echo "Your external IP: $USER_EXTERNAL_IP"
 echo "Local external IP: $LOCAL_EXTERNAL_IP"
 
-# Check if the user's external IP matches the resolved domain IP
+# Check if the user's external IP matches to the resolved domain name IP address, because they really should match!
 if [ "$USER_EXTERNAL_IP" != "$DOMAIN_IP" ]; then
-    read -r -p "Your external IP does not match the resolved domain IP. Do you want to continue? (Y/N): " CONTINUE
+    read -r -p "Your external IP does not match the resolved domain name IP address. I actually prefer that it does. Not certain if we should continue. Do you want to continue? (Y/N): " CONTINUE
     if [ "$CONTINUE" != "Y" ]; then
-        echo "Aborting script. No changes have been made."
+        echo "Aborting script. This is probably for the best. Make sure the domain name points to your server first. No changes have been made."
         exit 1
     fi
 fi
 
-# Install all the required packages for building the livestream nginx server. And also PHP.
+# Install all the required packages that are the building blocks for the live-stream server. Also installing PHP.
 if sudo apt install -y nginx software-properties-common dpkg-dev make gcc automake build-essential python3 python3-pip zlib1g-dev libpcre3 libpcre3-dev libssl-dev libxslt1-dev libxml2-dev libgd-dev libgeoip-dev libgoogle-perftools-dev libperl-dev pkg-config autotools-dev gpac ffmpeg mediainfo mencoder lame libvorbisenc2 libvorbisfile3 libx264-dev libvo-aacenc-dev libmp3lame-dev libopus-dev libnginx-mod-rtmp php-common php-fpm php-gd php-mysql php-imap php-cli php-cgi php-curl php-intl php-pspell php-sqlite3 php-tidy php-xmlrpc php8.1-xml php-memcache php-imagick php-zip php-mbstring php-pear mcrypt imagemagick memcached; then
     echo "Package installation successful."
 else
@@ -57,85 +64,89 @@ else
     exit 1
 fi
 
-# Configure php.ini
-sudo sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/8.1/fpm/php.ini
-
-# Add NGINX Mainline repository
-# Disabled since this requires user interaction
+# This adds the NGINX Mainline repository to upgrade Nginx to a more recent version.
+# We won't be doing that right now because it requires the user's input. Maybe some other time.
 # sudo add-apt-repository ppa:ondrej/nginx-mainline
 # sudo apt update && sudo apt upgrade -y
 
-# Create some directories
+# Configure php.ini so it will play nice with NginX
+sudo sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/8.1/fpm/php.ini
+
+# Create folders to store our web page and folders to store the live-stream temp files
 sudo mkdir -p /mnt/livestream/{hls,keys,dash,rec} /var/www/web
 
-# Delete or empty existing nginx.conf
+# Delete or empty the existing default ginx.conf file
 [ -f /etc/nginx/nginx.conf ] && sudo rm /etc/nginx/nginx.conf
 
-# Copy template nginx.conf. Confirm if file copy was succesfull or not
+# Copy the template nginx.conf to where it needs to be. Confirm if file copy was succesfull or not
 if sudo cp "conf/nginx.conf" "/etc/nginx/nginx.conf"; then
     echo "File copied successfully."
 else
-    echo "Error: Could not copy the file for some reason. Exiting."
+    echo "Error: Could not copy the file for some weird f*cked up reason. Sorry! Exiting."
     exit 1
 fi
 
-# Copy template vhost to new file and replace domain name
+# Using the entered domain name to create a new vhost file, adding domain name to it, and storing it where it needs to go.
 NEW_VHOST_FILE="/etc/nginx/sites-available/$DOMAIN_NAME.vhost"
 sudo cp "conf/vhost-template.vhost" "$NEW_VHOST_FILE"
 sudo sed -i "s/YOUR.HOSTNAME.COM/$DOMAIN_NAME/g" "$NEW_VHOST_FILE"
 
-# Enable the new vhost
+# Enabling the new vhost by creating a link to it.
 sudo ln -s "$NEW_VHOST_FILE" "/etc/nginx/sites-enabled/"
 
-# Disable the default vhost
+# Removing the link to the default vhost since we don't need it anymore.
 sudo rm "/etc/nginx/sites-enabled/default"
 
-# Clone nginx-rtmp-module
+# Cloning the official nginx-rtmp-module cause we need an important file from it 
 if sudo git clone https://github.com/arut/nginx-rtmp-module /usr/src/nginx-rtmp-module; then
-    # Copy stat.xsl to web root folder
+    # Copy the file stat.xsl to the web root folder
     sudo cp /usr/src/nginx-rtmp-module/stat.xsl /var/www/web/stat.xsl
 
-    # Copy some more files to web root folder
+    # Copy these files to the web root folder as well
     sudo cp "webfiles/index.html" "/var/www/web/index.html"
     sudo cp "webfiles/crossdomain.xml" "/var/www/web/crossdomain.xml"
     sudo cp "webfiles/robots.txt" "/var/www/web/robots.txt"
     sudo cp "webfiles/poster.jpg" "/var/www/web/poster.jpg"
 else
-    echo "Error: Could not clone or copy all the required files. Exiting."
+    echo "Error: Could not copy all the required files. This really sucks!! Exiting."
     exit 1
 fi
 
-# Change ownership of directories
+# Change ownership of the new folders so Nginx can write to them
 sudo chown -R www-data: /var/www/web /mnt/livestream
 
-# Install Snap core and Certbot
+# Install Snap core first so we can then install Certbot
 sudo snap install core; sudo snap refresh core
 sudo snap install --classic certbot
+# Create a link to certbot so we can use it later on
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
 
-# Add a line to visudo to restore original sudoers settings on EXIT
-echo "$USER ALL=(ALL) ALL" | sudo tee -a /etc/sudoers > /dev/null
-
-# Inform user about completion
-echo "Setup completed for $DOMAIN_NAME."
-echo "All the package have been successfully installed. Proceeding with certificate request."
+# Time to have a little chat with the user to let hum know what I've been doing.
+echo "Setup is pretty much completed for $DOMAIN_NAME."
+echo "All the package have been successfully installed. The very last step is to request a certificate."
+echo "I should be able to do this on my own. But I would appreciate it if you kept an eye on me, to see if everything went well."
 
 # Obtain the certificates.
 sudo certbot --nginx -d "$DOMAIN_NAME" --email "$EMAIL_ADDRESS" --agree-tos --noninteractive -m "$EMAIL_ADDRESS"
 
-echo "Completed the certificate request for $DOMAIN_NAME."
-echo "Proceeding with dhparam generator. May take a very long time to complete. Don't interrupt it!"
+# I feel that I should have another chat with our user. Communication is very important in a relationship!
+echo "Finished the certificate request for $DOMAIN_NAME."
+echo "Proceeding to generate the dhparam file. I'm doing the best I can, but this will take a long time to complete. Please don't interrupt me while I'm doing all the hard work!"
 
-# Generate DH parameters
+# Generate DH parameters and create the file in the nginx folder
 sudo openssl dhparam -out /etc/nginx/ssl-dhparams.pem 4096
 
-# Check if ssl-dhparams.pem file was created
+# Quick check if ssl-dhparams.pem file was created succesfully
 if [ -f /etc/nginx/ssl-dhparams.pem ]; then
-    echo "DH parameters generated successfully."
+    echo "DH parameters file generated successfully. Aren't you proud of me?"
 else
-    echo "Error: DH parameters could not be generated. Exiting."
+    echo "Error: DH parameters could not be generated. I'm so sorry about this! Exiting."
     exit 1
 fi
 
-# Add a line to visudo to restore original sudoers settings on EXIT
+# Add a line to visudo to restore the original settings in case the script in finished or on EXIT
 echo "$USER ALL=(ALL) ALL" | sudo tee -a /etc/sudoers > /dev/null
+
+echo "It seems everything was successful! This means I've completed all my tasks!"
+echo "Make sure that you take a look at the included docs to familiarize yourself with the server."
+echo "It has been a real pleasure. I am outta here! Goodye."
